0cdab43a0722be35b178da54ca13653258acaf9c
Murray.Stewart <Murray.stewart@and.digital>
2022-08-29 17:42:51 +0100

    selective tests test2

--- test2/run_test.go
+++ test2/run_test.go
@@ -9,3 +9,10 @@ func TestSayBar(t *testing.T) {
 		t.Errorf("expected: %s; got %s", want, got)
 	}
 }
+func TestTestSelection(t *testing.T) {
+	got := TestSelection()
+	want := "Baz"
+	if got != want {
+		t.Errorf("expected: %s; got %s", want, got)
+	}
+}
