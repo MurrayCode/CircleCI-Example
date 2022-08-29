9fe669d4eea1332c83048cc975ff7c2ee94329c6
Murray.Stewart <Murray.stewart@and.digital>
2022-08-29 17:29:28 +0100

    test pm;y rim tests in test1

--- test1/run.go
+++ test1/run.go
@@ -3,3 +3,7 @@ package test1
 func SayFoo() string {
 	return "Foo"
 }
+
+func TestSelection() string {
+	return "Baz"
+}
--- test1/run_test.go
+++ test1/run_test.go
@@ -9,3 +9,11 @@ func TestSayFoo(t *testing.T) {
 		t.Errorf("expected: %s; got %s", want, got)
 	}
 }
+
+func TestTestSelection(t *testing.T) {
+	got := TestSelection()
+	want := "Baz"
+	if got != want {
+		t.Errorf("expected: %s; got %s", want, got)
+	}
+}
